7056151f065195cedfc570da41314b16feb2f6b4
Administrator <Administrator@STAR-PC>
2017-06-12 14:31:07 +0800

    添加学习要点结构

--- Geodatabase/Form1.Designer.cs
+++ Geodatabase/Form1.Designer.cs
@@ -55,9 +55,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 366);
+            this.button1.Location = new System.Drawing.Point(78, 293);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(163, 23);
+            this.button1.Size = new System.Drawing.Size(122, 30);
             this.button1.TabIndex = 0;
             this.button1.Text = "连接空间库";
             this.button1.UseVisualStyleBackColor = true;
@@ -65,9 +66,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(429, 13);
+            this.button2.Location = new System.Drawing.Point(321, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.Size = new System.Drawing.Size(112, 30);
             this.button2.TabIndex = 1;
             this.button2.Text = "创建企业级数据库";
             this.button2.UseVisualStyleBackColor = true;
@@ -75,89 +77,100 @@
             // 
             // txt_server
             // 
-            this.txt_server.Location = new System.Drawing.Point(104, 17);
+            this.txt_server.Location = new System.Drawing.Point(78, 14);
+            this.txt_server.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_server.Name = "txt_server";
-            this.txt_server.Size = new System.Drawing.Size(163, 25);
+            this.txt_server.Size = new System.Drawing.Size(123, 21);
             this.txt_server.TabIndex = 2;
             // 
             // txt_instance
             // 
-            this.txt_instance.Location = new System.Drawing.Point(104, 70);
+            this.txt_instance.Location = new System.Drawing.Point(78, 56);
+            this.txt_instance.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_instance.Name = "txt_instance";
-            this.txt_instance.Size = new System.Drawing.Size(163, 25);
+            this.txt_instance.Size = new System.Drawing.Size(123, 21);
             this.txt_instance.TabIndex = 3;
             // 
             // txt_database
             // 
-            this.txt_database.Location = new System.Drawing.Point(104, 123);
+            this.txt_database.Location = new System.Drawing.Point(78, 98);
+            this.txt_database.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_database.Name = "txt_database";
-            this.txt_database.Size = new System.Drawing.Size(163, 25);
+            this.txt_database.Size = new System.Drawing.Size(123, 21);
             this.txt_database.TabIndex = 4;
             // 
             // txt_user
             // 
-            this.txt_user.Location = new System.Drawing.Point(104, 176);
+            this.txt_user.Location = new System.Drawing.Point(78, 141);
+            this.txt_user.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_user.Name = "txt_user";
-            this.txt_user.Size = new System.Drawing.Size(163, 25);
+            this.txt_user.Size = new System.Drawing.Size(123, 21);
             this.txt_user.TabIndex = 5;
             // 
             // txt_password
             // 
-            this.txt_password.Location = new System.Drawing.Point(104, 229);
+            this.txt_password.Location = new System.Drawing.Point(78, 183);
+            this.txt_password.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(163, 25);
+            this.txt_password.Size = new System.Drawing.Size(123, 21);
             this.txt_password.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(20, 17);
+            this.label1.Location = new System.Drawing.Point(15, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.Size = new System.Drawing.Size(41, 12);
             this.label1.TabIndex = 7;
             this.label1.Text = "server";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 73);
+            this.label2.Location = new System.Drawing.Point(17, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(71, 15);
+            this.label2.Size = new System.Drawing.Size(53, 12);
             this.label2.TabIndex = 8;
             this.label2.Text = "instance";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 129);
+            this.label3.Location = new System.Drawing.Point(17, 103);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 15);
+            this.label3.Size = new System.Drawing.Size(53, 12);
             this.label3.TabIndex = 9;
             this.label3.Text = "database";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(26, 186);
+            this.label4.Location = new System.Drawing.Point(20, 149);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(39, 15);
+            this.label4.Size = new System.Drawing.Size(29, 12);
             this.label4.TabIndex = 10;
             this.label4.Text = "user";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(27, 239);
+            this.label5.Location = new System.Drawing.Point(20, 191);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(71, 15);
+            this.label5.Size = new System.Drawing.Size(53, 12);
             this.label5.TabIndex = 11;
             this.label5.Text = "password";
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(429, 123);
+            this.button3.Location = new System.Drawing.Point(321, 104);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.Size = new System.Drawing.Size(112, 30);
             this.button3.TabIndex = 12;
             this.button3.Text = "创建要素数据集";
             this.button3.UseVisualStyleBackColor = true;
@@ -165,9 +178,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(615, 121);
+            this.button4.Location = new System.Drawing.Point(461, 104);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(120, 23);
+            this.button4.Size = new System.Drawing.Size(104, 27);
             this.button4.TabIndex = 13;
             this.button4.Text = "导入要素类";
             this.button4.UseVisualStyleBackColor = true;
@@ -175,9 +189,10 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(615, 248);
+            this.button5.Location = new System.Drawing.Point(461, 206);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.Size = new System.Drawing.Size(104, 27);
             this.button5.TabIndex = 14;
             this.button5.Text = "导出到gdb文件";
             this.button5.UseVisualStyleBackColor = true;
@@ -185,9 +200,10 @@
             // 
             // btn_fromgdb
             // 
-            this.btn_fromgdb.Location = new System.Drawing.Point(429, 248);
+            this.btn_fromgdb.Location = new System.Drawing.Point(321, 204);
+            this.btn_fromgdb.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_fromgdb.Name = "btn_fromgdb";
-            this.btn_fromgdb.Size = new System.Drawing.Size(149, 23);
+            this.btn_fromgdb.Size = new System.Drawing.Size(112, 30);
             this.btn_fromgdb.TabIndex = 15;
             this.btn_fromgdb.Text = "从gdb文件导入";
             this.btn_fromgdb.UseVisualStyleBackColor = true;
@@ -195,25 +211,28 @@
             // 
             // txt_featuredatasetname
             // 
-            this.txt_featuredatasetname.Location = new System.Drawing.Point(104, 289);
+            this.txt_featuredatasetname.Location = new System.Drawing.Point(78, 231);
+            this.txt_featuredatasetname.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_featuredatasetname.Name = "txt_featuredatasetname";
-            this.txt_featuredatasetname.Size = new System.Drawing.Size(163, 25);
+            this.txt_featuredatasetname.Size = new System.Drawing.Size(123, 21);
             this.txt_featuredatasetname.TabIndex = 16;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(16, 292);
+            this.label6.Location = new System.Drawing.Point(12, 234);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 15);
+            this.label6.Size = new System.Drawing.Size(65, 12);
             this.label6.TabIndex = 17;
             this.label6.Text = "要素集名称";
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(650, 19);
+            this.button6.Location = new System.Drawing.Point(488, 15);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(156, 23);
+            this.button6.Size = new System.Drawing.Size(131, 27);
             this.button6.TabIndex = 18;
             this.button6.Text = "遍历工作空间要素类";
             this.button6.UseVisualStyleBackColor = true;
@@ -222,16 +241,18 @@
             // FeatureClassBox
             // 
             this.FeatureClassBox.FormattingEnabled = true;
-            this.FeatureClassBox.Location = new System.Drawing.Point(848, 18);
+            this.FeatureClassBox.Location = new System.Drawing.Point(636, 14);
+            this.FeatureClassBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.FeatureClassBox.Name = "FeatureClassBox";
-            this.FeatureClassBox.Size = new System.Drawing.Size(171, 23);
+            this.FeatureClassBox.Size = new System.Drawing.Size(143, 20);
             this.FeatureClassBox.TabIndex = 19;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(429, 182);
+            this.button7.Location = new System.Drawing.Point(321, 152);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(149, 23);
+            this.button7.Size = new System.Drawing.Size(112, 30);
             this.button7.TabIndex = 20;
             this.button7.Text = "删除要素集";
             this.button7.UseVisualStyleBackColor = true;
@@ -239,9 +260,10 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(778, 182);
+            this.button8.Location = new System.Drawing.Point(584, 155);
+            this.button8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(115, 23);
+            this.button8.Size = new System.Drawing.Size(100, 27);
             this.button8.TabIndex = 21;
             this.button8.Text = "删除表C";
             this.button8.UseVisualStyleBackColor = true;
@@ -249,9 +271,10 @@
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(615, 182);
+            this.button9.Location = new System.Drawing.Point(461, 155);
+            this.button9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(120, 23);
+            this.button9.Size = new System.Drawing.Size(104, 27);
             this.button9.TabIndex = 22;
             this.button9.Text = "删除要素";
             this.button9.UseVisualStyleBackColor = true;
@@ -259,9 +282,9 @@
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1221, 463);
+            this.ClientSize = new System.Drawing.Size(916, 370);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
@@ -285,6 +308,7 @@
             this.Controls.Add(this.txt_server);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
